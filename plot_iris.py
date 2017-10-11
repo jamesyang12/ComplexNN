0e570f70e82524a11320062eb0f10491bb337046
James <jamesyang00712@gmail.com>
2017-10-11 02:35:56 -0400

    6 Plots of SVM on 4 features of Iris data

--- plot_iris.py
+++ plot_iris.py
@@ -33,7 +33,7 @@ kernel and its parameters.
    more realistic high-dimensional problems.
 
 """
-print(__doc__)
+#print(__doc__)
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -80,41 +80,94 @@ def plot_contours(ax, clf, xx, yy, **params):
 # import some data to play with
 iris = datasets.load_iris()
 # Take the first two features. We could avoid this by using a two-dim dataset
-X = iris.data[:, :2]
+#X = iris.data[:, :2]
 y = iris.target
+
+####
+'''
+    Data set:  Each species has 50 samples
+    Three species, each species has four features
+
+    Species: Setosa, Versicolour, and Virginica
+    Features: Sepal Length, Sepal Width, Petal Length and Petal Width
+
+'''
+####
+
+X_all = iris.data
+print("Total --> ", X_all)
+print("The data shape --> ", X_all.shape)
+print("The target shape --> ", y.shape)
+print(y)
+
 
 # we create an instance of SVM and fit out data. We do not scale our
 # data since we want to plot the support vectors
 C = 1.0  # SVM regularization parameter
-models = (svm.SVC(kernel='linear', C=C),
-          svm.LinearSVC(C=C),
-          svm.SVC(kernel='rbf', gamma=0.7, C=C),
-          svm.SVC(kernel='poly', degree=3, C=C))
-models = (clf.fit(X, y) for clf in models)
 
-# title for the plots
+models = (svm.SVC(kernel='linear', C=C),
+          svm.SVC(kernel='linear', C=C),
+          svm.SVC(kernel='linear', C=C),
+          svm.SVC(kernel='linear', C=C),
+          svm.SVC(kernel='linear', C=C),
+          svm.SVC(kernel='linear', C=C))
+
 titles = ('SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel')
+          'SVC with linear kernel',
+          'SVC with linear kernel',
+          'SVC with linear kernel',
+          'SVC with linear kernel',
+          'SVC with linear kernel')
 
 # Set-up 2x2 grid for plotting.
-fig, sub = plt.subplots(2, 2)
+fig, sub = plt.subplots(3, 2)
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-X0, X1 = X[:, 0], X[:, 1]
-xx, yy = make_meshgrid(X0, X1)
+#X0, X1 = X[:, 0], X[:, 1]
+#xx, yy = make_meshgrid(X0, X1)
+i=0
+j=1
 
 for clf, title, ax in zip(models, titles, sub.flatten()):
-    plot_contours(ax, clf, xx, yy,
-                  cmap=plt.cm.coolwarm, alpha=0.8)
+    if i==0:
+      ax.set_xlabel('Sepal length')
+    elif i==1:
+      ax.set_xlabel('Sepal width')
+    elif i==2:
+      ax.set_xlabel('Petal length')
+    elif i==3:
+      ax.set_xlabel('Peta width')
+
+    if j==0:
+      ax.set_ylabel('Sepal length')
+    elif j==1:
+      ax.set_ylabel('Sepal width')
+    elif j==2:
+      ax.set_ylabel('Petal length')
+    elif j==3:
+      ax.set_ylabel('Petal width')
+
+    X0, X1 = X_all[:, i], X_all[:, j]
+    xx, yy = make_meshgrid(X0, X1)
+
+    clf = svm.SVC(kernel='linear', C=C)
+    XXX = np.column_stack((iris.data[:,i], iris.data[:,j]))
+    clf.fit(XXX, y) 
+
+    plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
     ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
     ax.set_xlim(xx.min(), xx.max())
     ax.set_ylim(yy.min(), yy.max())
-    ax.set_xlabel('Sepal length')
-    ax.set_ylabel('Sepal width')
+    #ax.set_xlabel('Sepal length')
+    #ax.set_ylabel('Sepal width')
     ax.set_xticks(())
     ax.set_yticks(())
     ax.set_title(title)
+    
+    if j>=3:
+      i = i + 1
+      j = i + 1
+    else:
+      j = j + 1
 
 plt.show()
